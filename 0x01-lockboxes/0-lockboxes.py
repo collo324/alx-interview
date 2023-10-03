a433d1ddb917791811ffdebb1ec679909b1cfd5c
collo324 <emmanuellekishon1998@gmail.com>
2023-10-03 04:55:16 -0700

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-Prototype: def canUnlockAll(boxes)
+
+
+def canUnlockAll(boxes);
 Each box is numbered sequentially from 0 to n - 1
-{
-        boxes = n;
-        boxes = [[0], [2], [3], [4], []];
-        {
-            print = (boxes);
-            return = False;
-        }
-}
+'''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
