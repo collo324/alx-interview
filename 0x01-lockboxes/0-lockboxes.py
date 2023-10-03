21b0f91d66d92b64170f1020d243ebbe39f1b4aa
collo324 <emmanuellekishon1998@gmail.com>
2023-10-03 04:09:20 -0700

    Hot fix

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+Prototype: def canUnlockAll(boxes)
+Each box is numbered sequentially from 0 to n - 1
+{
+        boxes = n;
+        boxes = [[0], [2], [3], [4], []];
+        {
+            print = (boxes);
+            return = False;
+        }
+}
